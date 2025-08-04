3f5add0fbebd670ac7dd919a3822ca4768c7b0ae
ofikur <ofikur@users.noreply.github.com>
2025-08-04 17:21:40 +0700

    fix error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,12 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // UBAH DARI '/apidev' MENJADI '/api'
       '/api': {
         target: 'https://equran.id',
         changeOrigin: true,
       },
-      // Proxy untuk Al-Quran tetap sama dan benar
       '/api/v2': {
         target: 'https://equran.id',
         changeOrigin: true,
